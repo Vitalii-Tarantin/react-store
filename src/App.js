c65e179ec62d0d6a275c433a972c81a2c703b795
Vitalii <72747707+Vitalii-Tarantin@users.noreply.github.com>
2021-09-13 21:46:19 +0300

    create upload items in back-end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from 'react';
 import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
-const arr = [
-  { title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-  price: 7873, imageUrl: '/img/sneakers/1.jpg', },
-  { title: 'Мужские Кроссовки Nike Air Max 270',
-  price: 9899, imageUrl: '/img/sneakers/2.jpg',  },
-  { title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-  price: 7243, imageUrl: '/img/sneakers/3.jpg',  },
-  { title: 'Кроссовки Puma X Aka Boku Future Rider',
-  price: 5820, imageUrl: '/img/sneakers/4.jpg',  },
-];
-
 function App() {
+  const [items, setItems] = React.useState([]);
   const [cartOpened, setCartOpened] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch('https://613f8e11e9d92a0017e17778.mockapi.io/items')
+    .then(res => {
+      return res.json();
+    })
+    .then(json => {
+      setItems(json)
+    });
+  }, []); 
+
   return (
     <div className="wrapper clear">
-     
-      {cartOpened ? <Drawer onClose={() => setCartOpened(false)}  /> : null}
+     {cartOpened && <Drawer onClose={() => setCartOpened(false)}  />}
       <Header 
       onClickCart={() => setCartOpened(true)} 
       />
@@ -32,8 +32,8 @@ function App() {
           </div>
         </div>
 
-        <div className="d-flex">
-           {arr.map((obj) => (
+        <div className="d-flex flex-wrap">
+           {items.map((obj) => (
               <Card 
                 title= {obj.title}
                 price={obj.price} 
